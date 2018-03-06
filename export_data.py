b4ed16b3b9836b88a21a640b473f14ef2cf34695
ssi-victoriano-eco <victoriano.eco@surveysampling.com>
2018-03-06 22:41:55 +0800

    fix sample export script

--- export_data.py
+++ export_data.py
@@ -2,9 +2,10 @@ from Confirmit.DatabaseType import DatabaseType
 from Confirmit.WebServices.Authoring import Authoring
 from Confirmit.WebServices.SurveyData import SurveyData
 
+import time, json
 import zeep
 
-def GetConfirmitData(projectId):
+def GetConfirmitData(surveydata, projectId):
   token = None
   result = None
 
@@ -20,7 +21,7 @@ def GetConfirmitData(projectId):
     token = result.ResponseToken
     dataset = result.Result._value_1
     records = dataset.xpath('NewDataSet/responseid')
-    
+
     for record in records:
       children = record.iterchildren()
       response = {}
@@ -29,25 +30,21 @@ def GetConfirmitData(projectId):
         value = child.text
         response[key] = value
       confirmit_data.append(response)
-    
-    print(confirmit_data)
 
     if bool(token.LastDataSet):
       break
-  
+
   return confirmit_data
 
 if __name__ == '__main__':
   projectId = 'p1590365'
 
-  authoring = Authoring()
+  start = time.time()
   surveydata = SurveyData()
 
-  exists = authoring.ProjectExists(projectId)
+  data = GetConfirmitData(surveydata, projectId)
+  print('elapsed: ', time.time() - start)
 
-  if exists:
-    data = GetConfirmitData(projectId)
-    print(data)
-  else:
-    print('cannot find {}'.format(projectId))
-
+  # write to file
+  with open('./.exports/{}.json'.format(projectId), 'w') as f:
+    f.write(json.dumps(data))
